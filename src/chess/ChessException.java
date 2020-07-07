0818f318c96b5bd6949eabfb79a21355bf36c891
joana sara <joanasousa678@gmail.com>
2020-07-07 20:20:57 -0300

    added colors to pieces

---
+++ src/chess/ChessException.java
@@ -0,0 +1,21 @@
+package chess;
+
+public class ChessException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	  public ChessException(String msg) {
+		  super(msg);
+	  }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.Pieces.King;
 import chess.Pieces.Rook;
 
@@ -26,13 +25,28 @@ public class ChessMatch {
 		return mat;
 	}
    
+	private void placeNewPice(char calumn, int row, ChessPiece piece) {
+		board.placePiece(piece,new ChessPsition(calumn, row).toPosition());
+	}
+	
+	
+	
+	
 	private void initialSatup() {
-		
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board,Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board,Color.WHITE), new Position(7,4));
-		
-		
+		placeNewPice('c', 1, new Rook(board, Color.WHITE));
+        placeNewPice('c', 2, new Rook(board, Color.WHITE));
+        placeNewPice('d', 2, new Rook(board, Color.WHITE));
+        placeNewPice('e', 2, new Rook(board, Color.WHITE));
+        placeNewPice('e', 1, new Rook(board, Color.WHITE));
+        placeNewPice('d', 1, new King(board, Color.WHITE));
+
+        placeNewPice('c', 7, new Rook(board, Color.BLACK));
+        placeNewPice('c', 8, new Rook(board, Color.BLACK));
+        placeNewPice('d', 7, new Rook(board, Color.BLACK));
+        placeNewPice('e', 7, new Rook(board, Color.BLACK));
+        placeNewPice('e', 8, new Rook(board, Color.BLACK));
+        placeNewPice('d', 8, new King(board, Color.BLACK));
+	}
 		
 		
 		
@@ -49,4 +63,4 @@ public class ChessMatch {
 		
 		
 	}
-}
+
